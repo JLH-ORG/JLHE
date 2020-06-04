a3cd72d061eb680bb1e9966ec9cdb7528cb2739e
Harley Reeves-Martin <50149947+harley-rm@users.noreply.github.com>
2020-06-04 14:57:04 +0100

    Removed unnecessary #include in entry point.

--- JLHE/src/JLHE/EntryPoint.h
+++ JLHE/src/JLHE/EntryPoint.h
@@ -2,14 +2,11 @@
 
 #ifdef JLHE_PLATFORM_WINDOWS
 
-#include <glad/glad.h>
-
 	extern JLHE::Application* JLHE::CreateApplication();
 
 	int main(int argc, char** argv) {
 		JLHE::Log::Init();
 		JLHE_CORE_TRACE("Initalized log.");
-		JLHE_CORE_TRACE("GL Version: {0}", glGetString(GL_VERSION));
 
 		auto app = JLHE::CreateApplication();
 		app->Run();
