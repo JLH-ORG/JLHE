47a21602466d1c3c41dd2fd97a3daa83d7a369f5
Lewis-McKenzie <53063230+Lewis-McKenzie@users.noreply.github.com>
2020-06-23 13:58:18 +0100

    Added Stats to 2D Renderer

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -22,6 +22,8 @@ void Sandbox2D::OnDetach() {
 void Sandbox2D::OnUpdate(JLHE::Timestep ts) {
 	JLHE_PROFILE_FUNCTION();
 
+	JLHE::Renderer2D::ResetStats();
+
 	// Update
 	m_CameraController.OnUpdate(ts);
 
@@ -37,7 +39,13 @@ void Sandbox2D::OnUpdate(JLHE::Timestep ts) {
 		JLHE::Renderer2D::DrawQuad({ 1.0f, 1.0f }, { 0.5f, 1.0f }, m_SquareColor);
 		JLHE::Renderer2D::DrawRotatedQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, 45.0f, m_CheckerboardTexture, 10.0f, glm::vec4(1.0f, 0.9f, 0.9f, 1.0f));
 		JLHE::Renderer2D::DrawQuad({ -5.0f, -5.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
-		//JLHE::Renderer2D::DrawRotatedQuad({ 0.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, 45.0f, m_CheckerboardTexture, 20.0f);
+		JLHE::Renderer2D::DrawRotatedQuad({ 2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, 45.0f, m_CheckerboardTexture, 20.0f);
+		for (float y = -5.0f; y < 5.0f; y += 0.5f) {
+			for (float x = -5.0f; x < 5.0f; x += 0.5f) {
+				glm::vec4 color = { (x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.7f };
+				JLHE::Renderer2D::DrawQuad({ x, y }, { 0.45f, 0.45f }, color);
+			}
+		}
 		JLHE::Renderer2D::EndScene();
 	}
 }
@@ -46,6 +54,14 @@ void Sandbox2D::OnImGuiRender() {
 	JLHE_PROFILE_FUNCTION();
 
 	ImGui::Begin("Settings");
+
+	auto stats = JLHE::Renderer2D::GetStats();
+	ImGui::Text("Renderer2D Stats:");
+	ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+	ImGui::Text("Quads: %d", stats.QuadCount);
+	ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
+	ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
+
 	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
 	ImGui::End();
 }
